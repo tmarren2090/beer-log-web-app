dd5b6201ebd219f4b534ca914cf95728516479ed
Thomas Marren <tmarren2090@gmail.com>
2015-11-05 18:25:55 -0500

    change all table rows to input fields and add php script to edit each table cell

--- index.php
+++ index.php
@@ -6,15 +6,17 @@
 <script type="text/javascript" src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 <script type="text/javascript" src="jquery.tablesorter.js"></script>
 
-
 <script type="text/javascript">
-  
+
 $(document).ready(function() {
     $("table").tablesorter({ 
         // initial sort on the first column, order asc 
         sortList: [[0,0]]
 });
 });
+
+  
+  
   
 </script>
 
@@ -51,6 +53,14 @@ require_once('core/functions.php');
 
 $link = f_sqlConnect (DB_USER, DB_PASSWORD, DB_NAME);
 
+if( isset($_POST['update'])){
+
+  $updateQuery = 'UPDATE beer_table SET beer="$_POST[beer]", brewery="$_POST[brewery]", city="$_POST[city]", state_country="$_POST[state_country]", type="$_POST[type]", style="$_POST[style]", abv="$_POST[abv]", rating="$_POST[rating]", comments="$_POST[comments]" WHERE beer="$_POST[hidden]"';
+  mysql_query($updateQuery, mysql_connect('localhost', 'beer', 'root'));
+
+};
+
+
 $sql = 'SELECT * FROM beer_table';
 
 $results = mysql_query($sql);
@@ -65,7 +75,7 @@ echo '<div class="container">';
 echo '<table border="1" style="width:100%" id="myTable" class="tablesorter">';
 echo '<thead>
       <tr>
-      <th id="beer"><a href="#">Beer<i class="fa fa-sort"></i></a></th>
+      <th id="beer"><a href="#" onclick="return false;">Beer<i class="fa fa-sort"></i></a></th>
       <th id="brewery"><a href="#">Brewery<i class="fa fa-sort"></i></a></th> 
       <th id="city"><a href="#">City<i class="fa fa-sort"></i></a></th>
       <th id="state"><a href="#">State/Country<i class="fa fa-sort"></i></a></th>
@@ -74,25 +84,32 @@ echo '<thead>
       <th id="abv"><a href="#">ABV%<i class="fa fa-sort"></i></a></th>
       <th id="rating"><a href="#">Rating<i class="fa fa-sort"></i></a></th>
       <th id="comments"><a href="#">Comments<i class="fa fa-sort"></i></a></th>
+      <th id="edit">Edit</th>
       </tr>
       </thead>
       <tbody>';
 
 while($result = mysql_fetch_array( $results )){
 
+  echo '<form action="index.php" method="post">';
+  
   echo '<tr>';
   
-  echo '<td>' . $result['beer'] . '</td>';
-  echo '<td>' . $result['brewery'] . '</td>';
-  echo '<td>' . $result['city'] . '</td>';
-  echo '<td>' . $result['state_country'] . '</td>';
-  echo '<td>' . $result['type'] . '</td>';
-  echo '<td>' . $result['style'] . '</td>';
-  echo '<td>' . $result['abv'] . '</td>';
-  echo '<td>' . $result['rating'] . '</td>';
-  echo '<td>' . $result['comments'] . '</td>';
+  echo '<td>' . '<input type="text" name="beer" value="' . $result['beer'] . '"> </td>';
+  echo '<td>' . '<input type="text" name="brewery" value="' . $result['brewery'] . '" </td>';
+  echo '<td>' . '<input type="text" name="city" value="' . $result['city'] . '" </td>';
+  echo '<td>' . '<input type="text" name="state_country" value="' . $result['state_country'] . '" </td>';
+  echo '<td>' . '<input type="text" name="type" value="' . $result['type'] . '" </td>';
+  echo '<td>' . '<input type="text" name="style" value="' . $result['style'] . '" </td>';
+  echo '<td>' . '<input type="text" name="abv" value="' . $result['abv'] . '" </td>';
+  echo '<td>' . '<input type="text" name="rating" value="' . $result['rating'] . '" </td>';
+  echo '<td>' . '<input type="text" name="comments" value="' . $result['comments'] . '" </td>';
+  echo '<td>' . '<input type="hidden" name="hidden" value="' . $result['beer'] . '" </td>';
+  echo '<td>' . '<input type="submit" name="update" id="editIcon" value="&#xf044;"</td>';
   
   echo '</tr>';
+  
+  echo '</form>';
 
 }
 
